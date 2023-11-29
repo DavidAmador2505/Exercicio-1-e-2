049a08c1e12f8502459ee219db717ed84c6663a1
David Amador <davidamador2505@gmail.com>
2023-11-29 11:11:41 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Exercicio_1_e_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(198, 66);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(178, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(73, 146);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 45);
+            button1.TabIndex = 2;
+            button1.Text = "Clicar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(408, 159);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(62, 19);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Aceitar";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(408, 193);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(87, 19);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Não Aceitar";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(73, 218);
+            button2.Name = "button2";
+            button2.Size = new Size(103, 58);
+            button2.TabIndex = 5;
+            button2.Text = "Data";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(233, 218);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 58);
+            button3.TabIndex = 6;
+            button3.Text = "Limpar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+namespace Exercicio_1_e_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                textBox1.Text = "Bem vindo ao C#!";
+            }
+            else if (radioButton2.Checked)
+            {
+                textBox1.Text = "É necessário aceitar!";
+            }
+            else
+            {
+                textBox1.Text = "É obrigatorio selecionar um deles!";
+            }
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+        }
+    }
+}
